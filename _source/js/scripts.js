bf74b1dd383e76f83fe46b64e660c60df354e01f
a-zharikov <skreets@mail.ru>
2022-03-07 22:20:34 +0300

    sidebar final

--- _source/js/scripts.js
+++ _source/js/scripts.js
@@ -1,8 +1,12 @@
 $(document).ready(function() {
-  
+
   $(document).on('click', '.theme', function(){
     $(this).toggleClass('active');
     $('html').toggleClass('dark');
+  });
+
+  $(document).on('click', '.burder', function(){
+    $('.sidebar').toggleClass('active');
   });
 
 });
