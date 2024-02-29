33b5c1142b993c1467c694eba0770e7d278221c3
Alexander <87537038+a-zharikov@users.noreply.github.com>
2024-02-29 16:17:36 +0300

    error page

---
+++ components/error/404.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="error-page">
+    <h1>404</h1>
+    <p>Эта страница не существует</p>
+    <nuxt-link to="/">Главная страница</nuxt-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'error-404',
+  props: {
+    error: {
+      type: Object,
+      default: () => {},
+    },
+  },
+  data() {
+      return {
+        title: '404 // Zharikov.design'
+      }
+    },
+    head() {
+      return {
+        title: this.title,
+        meta: [
+          {
+            hid: 'description',
+            name: 'description',
+            content: 'Ошибка 404'
+          },
+          {
+            hid: 'keywords',
+            name: 'keywords',
+            content: 'страница, не, существует'
+          }
+        ]
+      }
+    }
+};
+</script>
---
+++ components/error/500.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="error-page">
+    <h1>500</h1>
+    <p>Внутренняя Ошибка Сервера</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'error-500',
+  props: {
+    error: {
+      type: Object,
+      default: () => {},
+    },
+  },
+  data() {
+      return {
+        title: '500 // Zharikov.design'
+      }
+    },
+    head() {
+      return {
+        title: this.title,
+        meta: [
+          {
+            hid: 'description',
+            name: 'description',
+            content: 'Ошибка 500'
+          },
+          {
+            hid: 'keywords',
+            name: 'keywords',
+            content: 'внутренняя, ошибка, сервера'
+          }
+        ]
+      }
+    }
+};
+</script>
